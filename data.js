9aa9cd41b56ad7103a10e8647631044de8b02e93
HATCHI SABILILLAH S <blackpanteq123@gmail.com>
2025-11-07 16:31:38 +0700

    data.js
    data java scrip untuk bagian pemessanan buku untuk memilih gambar buku

---
+++ data.js
@@ -0,0 +1,32 @@
+// js/data.js
+const dataKatalogBuku = [
+  { 
+    id: 1, 
+    judul: "Belajar HTML", 
+    penulis: "Andi", 
+    harga: 50000, 
+    stok: 10, 
+    gambar: "img/imgbuku1.png" 
+  },
+  { 
+    id: 2, 
+    judul: "JavaScript Dasar", 
+    penulis: "Budi", 
+    harga: 70000, 
+    stok: 8, 
+    gambar: "img/imgbuku2.png" 
+  },
+  { 
+    id: 3, 
+    judul: "CSS Modern", 
+    penulis: "Cici", 
+    harga: 60000, 
+    stok: 15, 
+    gambar: "img/imgbuku2.png"   // atau imgbuku1.png juga boleh
+  }
+];
+
+const dataTracking = [
+  { noDO: "DO123", nama: "Sinta", status: "Sedang dikirim", ekspedisi: "JNE", tanggal: "2025-11-05", paket: "Reguler", total: 120000, progress: 60 },
+  { noDO: "DO456", nama: "Rudi", status: "Sampai Tujuan", ekspedisi: "SiCepat", tanggal: "2025-11-03", paket: "Express", total: 85000, progress: 100 }
+];
